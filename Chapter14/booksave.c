d513cee080e500c9fbee8fdd5e8e04b94826c8b1
YiminXia <1275499353@qq.com>
2018-08-02 13:41:17 +0800

    first class

--- Chapter14/booksave.c
+++ Chapter14/booksave.c
@@ -11,9 +11,10 @@ struct book {
        char author[AUTHORLEN];
        float price  ;    
 };
-
+void writeTheBookInfoIntoTheFile(struct book *, int, FILE *);
+void readTheBookInfoFromTheFile(struct book *, int, FILE *);
 char * s_gets(char *, int );
-
+/*
 int main()
 {
    char * st = (char *) malloc(AUTHORLEN);
@@ -22,6 +23,65 @@ int main()
    printf("name is %s\n",st);
    return 0;
 }
+*/
+
+int main()
+{
+  int count = 0;
+  int index, fileCount;
+  FILE * pbooks;
+  struct book book1;
+  int theNumberOfBooks;
+
+  
+  if(NULL == (pbooks = fopen("books.dat", "a + b")))
+  {
+      fputs("open the file books.dat failed.\n", stderr);
+      exit(0);
+  }
+
+  struct book books[1]={
+   {"gone with wind",
+    "whatever",
+    12.87
+   }
+ };
+
+ rewind(pbooks);
+
+
+ writeTheBookInfoIntoTheFile(&books[0], 1, pbooks);
+
+ struct book * books2 = (struct book *)malloc(sizeof(struct book));
+ rewind(pbooks);
+ readTheBookInfoFromTheFile(books2, 1, pbooks);  
+ printf("name = %s author = %s and the value = %f\n", books2 -> title, books2 -> author, books2 -> price);
+ free(books2);
+ 
+  return 0;
+}
+
+void writeTheBookInfoIntoTheFile(struct book * ptr, int theNumberOfBooks, FILE * pbooks)
+{
+  int counter = 0; 
+  int size = sizeof(struct book);
+  while(counter < theNumberOfBooks && 1 == fwrite(ptr, size, 1, pbooks))
+  { 
+    counter++;
+  }
+ // printf("%d",fwrite(ptr, size, 1, pbooks));
+}
+
+void readTheBookInfoFromTheFile(struct book * ptr, int theNumberOfBooks, FILE * pbooks)
+{
+    int counter = 0;
+    int size = sizeof(struct book);
+    while(counter < theNumberOfBooks && 1 == fread(ptr, size, 1, pbooks))
+    {
+      counter++;
+    }
+}
+
 
 char * s_gets(char * st, int n)
 {
