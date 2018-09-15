58e0aa960147574d2ee04c620b0747a39023ec73
YiminXia <1275499353@qq.com>
2018-09-16 05:49:45 +0800

    a new file testStrlen.c and a modified file testNULL.c:

---
+++ Chapter10/testStrlen.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+
+  char strings[] = {"li da han bu hao."};
+  printf("strings length is: %d\n", strlen(strings));
+  printf("sizeof(strings) is: %d\n",sizeof(strings));
+  return 0;
+}
--- Chapter11/testNULL.c
+++ Chapter11/testNULL.c
@@ -1,9 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+
 int main(void)
 { 
   printf("NULL is : %p\n",NULL);
   //printf("&NULL is : %p\n",&NULL);
+  
   return 0;
 }
